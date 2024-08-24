cab267eb0da04f1426d1c88b27bd8fbb0c9c0038
celsopires1999 <celso.pires1999@gmail.com>
2024-08-23 21:14:12 -0300

    initial commit

---
+++ .docker/start.dev.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# if [ ! -f "./envs/.env" ]; then
+#     cp ./envs/.env.example ./envs/.env
+# fi
+
+
+# if [ ! -f "./envs/.env.test" ]; then
+#     cp ./envs/.env.test.example ./envs/.env.test
+# fi
+
+# if [ ! -f "./envs/.env.e2e" ]; then
+#     cp ./envs/.env.e2e.example ./envs/.env.e2e
+# fi
+
+npm install
+
+tail -f /dev/null
+
+# npm run start:dev
